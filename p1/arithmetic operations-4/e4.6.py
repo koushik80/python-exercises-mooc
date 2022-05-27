fc9ca50c3d39af22706026d715544ba8104c4967
Koushik Dey <koushik.dey@live.com>
2022-05-27 15:07:44 +0300

    e4.6s

---
+++ p1/arithmetic operations-4/e4.6.py
@@ -0,0 +1,19 @@
+#E4.6:Sum and product
+
+#Problem: Please write a program which asks the user for two numbers.
+#The program will then print out the sum and the product of the two numbers.
+
+#The program should function as follows:
+
+            #Number 1: 3
+            #Number 2: 7
+            #The sum of the numbers: 10
+            #The product of the numbers: 21
+
+#Solution:
+
+num1 = int(input("Please give a number: "))
+num2 = int(input("Please give a number: "))
+print("The sum of the numbers:",num1 + num2)
+print("The product of the numbers:",num1 * num2)
+
