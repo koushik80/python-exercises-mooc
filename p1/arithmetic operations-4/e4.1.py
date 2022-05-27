a983f0210b24678cd6523efd440303f5d7050771
Koushik Dey <koushik.dey@live.com>
2022-05-27 12:23:23 +0300

    e4.2s

---
+++ p1/arithmetic operations-4/e4.1.py
@@ -0,0 +1,16 @@
+#E-4.1: Times five
+
+#Problem: Please write a program which asks the user for a number.
+#The program then prints out the number multiplied by five.
+
+#The program should function as follows:
+
+        #Please type in a number: 3
+        #3 times 5 is 15
+
+#Solution:
+
+
+
+
+
