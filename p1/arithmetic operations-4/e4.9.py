9cf284e7b67c80a4b2c60d278a3a9a947a382ab7
Koushik Dey <koushik.dey@live.com>
2022-05-29 16:40:36 +0300

    e5.1s

---
+++ p1/arithmetic operations-4/e4.9.py
@@ -0,0 +1,28 @@
+#E-4.9: Students in groups
+
+#Problem: Please write a program which asks for the number of students on a course and the desired group size.
+#The program will then print out the number of groups formed from the students on the course.
+#If the division is not even, one of the groups may have fewer members than specified.
+
+#If you can't get your code working as expected,
+#it is absolutely okay to move on and come back to this exercise later.
+#The topic of the next section is conditional statements.
+#This exercise can also be solved using a conditional construction.
+
+            #How many students on the course? 8
+            #Desired group size? 4
+            #Number of groups formed: 2
+
+            #How many students on the course? 11
+            #Desired group size? 3
+            #Number of groups formed: 4
+
+
+#Solution:
+
+#num_students = int(input("How many students on the course?"))
+#size_groups = int(input("Desired group size?"))
+#group_form = int(num_students / size_groups)
+
+
+
---
+++ p1/conditional_statements-5/e5.1.py
@@ -0,0 +1,19 @@
+#Please write a program which asks the user for an integer number.
+#The program should print out "Orwell" if the number is exactly 1984,
+#and otherwise do nothing.
+
+            #Please type in a number: 2020
+
+            #Please type in a number: 1984
+            #Orwell
+
+#Solution:
+
+num = int(input("Please type in a number: "))
+
+if num == 1984:
+    print("Orwell")
+else:
+    print()
+
+
