bf64e5479b80f536d8ee0f4f8c455f3b183cd3ce
Koushik Dey <koushik.dey@live.com>
2022-05-29 21:30:32 +0300

    e5.4.s

---
+++ p1/conditional_statements-5/e5.4.py
@@ -0,0 +1,34 @@
+#E-5.4:Please write a program which asks the user for an integer number.
+#The program should then print out the magnitude of the number according to the following examples.
+
+            #Please type in a number: 950
+            #This number is smaller than 1000
+            #Thank you!
+
+            #Please type in a number: 59
+            #This number is smaller than 1000
+            #This number is smaller than 100
+            #Thank you!
+
+            #Please type in a number: 2
+            #This number is smaller than 100
+            #This number is smaller than 10
+            #Thank you!
+
+            #Please type in a number: 1123
+            #Thank you!
+
+#Solution:
+
+num = int(input("Please type in a number:"))
+
+if num < 1000:
+    print("This number is smaller than 1000")
+if num < 100:
+    print("This number is smaller than 100")
+if num < 10:
+    print("This number is smaller than 10")
+if num > 1000:
+    print()
+
+print("Thank you!")
