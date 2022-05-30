b240379f6ea991ea2be235ef29d5db46e21e2888
Koushik Dey <koushik.dey@live.com>
2022-05-31 00:53:52 +0300

    :white_check_mark: e2.1.2s
    completed

---
+++ p2/Programming terminology/e2.1.2.py
@@ -0,0 +1,25 @@
+#E-2.1.2: Number of characters
+
+#Please write a program which asks the user for a word and then prints out the number of characters, if there was more than one typed in.
+
+#Examples of expected behaviour:
+
+        #Please type in a word: hey
+        #There are 3 letters in the word hey
+        #Thank you!
+
+        #Please type in a word: banana
+        #There are 6 letters in the word banana
+        #Thank you!
+
+        #Please type in a word: b
+        #Thank you!
+
+#Solution:
+
+word = str(input("Please type in a word: "))
+
+if len(word) > 1:
+    print(f"There are {len(word)} letters in the word {word}")
+
+print(f"Thank you!")
