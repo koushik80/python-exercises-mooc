1abdc82929a1c35eba2990c24cac3815454cab72
Koushik Dey <koushik.dey@live.com>
2022-05-30 13:10:48 +0300

    e5.7s

--- p1/conditional_statements-5/e5.7.py
+++ p1/conditional_statements-5/e5.7.py
@@ -1,7 +1,8 @@
 #E-5.7:Daily wages
 
 #Problem: Please write a program which asks for the hourly wage, hours worked, and the day of the week.
-#The program should then print out the daily wages, which equal hourly wage multiplied by hours worked,
+#The program should then print out the daily wages,
+#which equal hourly wage multiplied by hours worked,
 #except on Sundays when the hourly wage is doubled.
 
             #Hourly wage: 8.5
@@ -19,11 +20,54 @@
 wage = float(input("Hourly wage:"))
 work = int(input("Hours worked:"))
 option = input("Day of the week:")
-option2 = "Sunday"
-daily_wages = wage * work
+daily_wage = wage * work
+dbl = 2 * daily_wage
 
-if option == True:
-    print("Daily wages:",daily_wages)
-elif option2:
-    print("Daily wages:",daily_wages * 2)
 
+def Monday(wage, work):
+    return(wage * work)
+
+
+def Tuesday(wage, work):
+    return(wage * work)
+
+
+def Wednesday(wage, work):
+    return(wage * work)
+
+
+def Thursday(wage, work):
+    return(wage * work)
+
+
+def Friday(wage, work):
+    return(wage * work)
+
+
+def Saturday(wage, work):
+    return(wage * work)
+
+
+def Sunday(wage, work):
+    return((wage * work) * 2)
+
+
+result = 0
+
+
+if option == "Saturday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Friday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Monday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Tuesday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Wednesday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Thursday":
+    print("Daily wages:", daily_wage, "euros")
+elif option == "Sunday":
+    print("Daily wages:", dbl, "euros")
+else:
+    print()
