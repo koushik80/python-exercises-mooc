c21693a199333edd9d1669a31999b22ccb0ff21c
Koushik Dey <koushik.dey@live.com>
2022-05-31 01:10:51 +0300

    :white_check_mark: e5.2.2s
    solved

---
+++ p2/More conditionals/e2.2.1.py
@@ -0,0 +1,21 @@
+#E-2.2.1: Age of maturity
+
+#Problem:Please write a program which asks the user for their age.
+#The program should then print out a message based on whether the user is of age or not,
+#using 18 as the age of maturity.
+
+#Some examples of expected behaviour:
+
+        #How old are you? 12
+        #You are not of age!
+
+        #How old are you? 32
+        #You are of age!
+
+#Solution:
+
+age = int(input("How old are you? "))
+if age < 18:
+    print(f"You are not of age!")
+else:
+    print(f"You are of age!")
