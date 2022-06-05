4792c9be6e8658a7b7a275c73705b5446633bcb4
koushik <koushik.dey@live.com>
2022-06-06 00:06:58 +0300

    :white_check_mark: e3.1.2s
    solved

---
+++ p3/Loops with conditions/e3.1.2.py
@@ -0,0 +1,32 @@
+#E-3.1.2: Fix the code: Countdown
+
+#Problem: The program below has some syntactic issues:
+
+        #print("Are you ready?")
+        #number = int(input("Please type in a number: "))
+        #while number = 0:
+        #print(number)
+        #print("Now!")
+
+#Please fix it so that it prints out the following:
+
+        #Are you ready?
+        #Please type in a number: 5
+        #5
+        #4
+        #3
+        #2
+        #1
+        #Now!
+
+
+#Solution:
+
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+
+while number > 0:
+    print(number)
+    number = number - 1
+
+print("Now!")
