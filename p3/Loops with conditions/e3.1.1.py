ca05232f3033c705ff2877dfa6f8da67beb8cd46
koushik <koushik.dey@live.com>
2022-06-05 23:47:26 +0300

    :white_check_mark: e3.1.1s2
    solved

--- p3/Loops with conditions/e3.1.1.py
+++ p3/Loops with conditions/e3.1.1.py
@@ -1,4 +1,5 @@
-#E-3.1.1: Please write a program which prints out all the even numbers between two and thirty, using a loop.
+#E-3.1.1: Print numbers
+#Problem: Please write a program which prints out all the even numbers between two and thirty, using a loop.
 #Print each number on a separate line.
 
 #The beginning of your output should look like this:
