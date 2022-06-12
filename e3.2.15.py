4acb7cd20b5bdb49ddc490ed017a8b5254fdd528
koushik <koushik.dey@live.com>
2022-06-13 00:58:28 +0300

    :construction: e3.2.15s
    und

---
+++ e3.2.15.py
@@ -0,0 +1,43 @@
+#E-3.2.15: The second occurrence
+
+#Problem: Please write a program which finds the second occurrence of a substring.
+#If there is no second (or first) occurrence,
+#the program should print out a message accordingly.
+
+#In this exercise the occurrences cannot overlap.
+#For example, in the string aaaa the second occurrence of the substring aa is at index 2.
+
+#Some examples of expected behaviour:
+
+        #Please type in a string: abcabc
+        #Please type in a substring: ab
+        #The second occurrence of the substring is at index 3.
+
+        #Please type in a string: methodology
+        #Please type in a substring: o
+        #The second occurrence of the substring is at index 6.
+
+        #Please type in a string: aybabtu
+        #Please type in a substring: ba
+        #The substring does not occur twice in the string.
+
+#Solution:
+
+word = input("Please type in a word: ")
+char = input("Please type in a character: ")
+index = word.find(char)
+position = ""
+count = 0
+
+while index <= len(word):
+    if word[index] <= char:
+        count += 1
+        position = index
+        if count == 1:
+            break
+    index += 1
+
+if count == 2:
+    print(f"The second occurrence of the substring is at index {position}.")
+else:
+    print("The substring does not occur twice in the string.")
---
+++ p3/Working with strings/e3.2.12.py
@@ -0,0 +1,20 @@
+#E-3.2.12:Does it contain vowels
+
+#Problem:Please write a program which asks the user to input a string.
+#The program then prints out different messages if the string contains any of the vowels a, e or o.
+
+#You may assume the input will be in lowercase entirely.
+#Have a look at the examples below.
+
+        #Please type in a string: hello there
+        #a not found
+        #e found
+        #o found
+
+        #Please type in a string: hiya
+        #a found
+        #e not found
+        #o not found
+
+#Solution:
+
