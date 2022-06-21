47c7cb2b041685262fe2a853e0d04b00357d43f2
koushik <koushik.dey@live.com>
2022-06-21 21:42:16 +0300

    :construction: und
    und

---
+++ p3/More loops/e3.3.1.py
@@ -0,0 +1,33 @@
+#E-3.3.1:Multiplication
+
+#Problem: Please write a program which asks the user for a positive integer number.
+#The program then prints out a list of multiplication operations until both operands reach the number given by the user.
+#See the examples below for details:
+
+        #Please type in a number: 2
+        #1 x 1 = 1
+        #1 x 2 = 2
+        #2 x 1 = 2
+        #2 x 2 = 4
+
+        #Please type in a number: 3
+        #1 x 1 = 1
+        #1 x 2 = 2
+        #1 x 3 = 3
+        #2 x 1 = 2
+        #2 x 2 = 4
+        #2 x 3 = 6
+        #3 x 1 = 3
+        #3 x 2 = 6
+        #3 x 3 = 9
+
+#Solution:
+
+
+num = int(input("Please type in a number: "))
+i = 1
+while num % 2 == 0:
+    if i == num:
+        break
+    i += 1
+    print(num, 'x', i, '=', num * i)
