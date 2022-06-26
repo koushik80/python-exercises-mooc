794b06a46e38ec71df3ac13d6830c33d0f8fcd89
koushik <koushik.dey@live.com>
2022-06-26 21:31:26 +0300

    :white_check_mark: e3.4.6s
    done

---
+++ p3/Defining Functions/e3.4.4.py
@@ -0,0 +1,32 @@
+#E-3.4.4:Print many times
+
+#Problem: Please write a function named print_many_times(text, times),
+#which takes a string and an integer as arguments.
+#The integer argument specifies how many times the string argument should be printed out:
+
+        #print_many_times("hi", 5)
+
+        #print()
+
+        #text = "All Pythons, except one, grow up"
+        #times = 3
+        #print_many_times(text, times)
+
+#Sample output:
+
+        #hi
+        #hi
+        #hi
+        #hi
+        #hi
+
+        #All Pythons, except one, grow up.
+        #All Pythons, except one, grow up.
+        #All Pythons, except one, grow up.
+
+#Solution:
+
+
+       
+
+
---
+++ p3/Defining Functions/e3.4.6.py
@@ -0,0 +1,45 @@
+#E-3.4.6:Chessboard
+
+#Problem: Please write a function named chessboard,
+#which prints out a chessboard made out of ones and zeroes.
+#The function takes an integer argument,
+#which specifies the length of the side of the board.
+#See the examples below for details:
+
+        #chessboard(3)
+        #print()
+        #chessboard(6)
+
+        #101
+        #010
+        #101
+
+        #101010
+        #010101
+        #101010
+        #010101
+        #101010
+        #010101
+
+#Solution:
+
+def chessboard(n):
+    Sentence_1 = ""
+    Sentence_2 = ""
+    for i in range(n):
+        if i % 2 == 0:
+            Sentence_1 += "1"
+            Sentence_2 += "0"
+        else:
+            Sentence_1 += "0"
+            Sentence_2 += "1"
+
+    for i in range(n):
+        if i % 2 == 0:
+            print(Sentence_1)
+        else:
+            print(Sentence_2)
+
+
+if __name__ == "__main__":
+    chessboard(3)
