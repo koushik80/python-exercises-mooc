9b84cbd64c544d08322a546080d4e4fab1091e4c
koushik <koushik.dey@live.com>
2022-06-26 16:47:05 +0300

    :white_check_mark: e3.4.3s
    done

---
+++ p3/Defining Functions/e3.4.3.py
@@ -0,0 +1,20 @@
+#E-3.4.3: Mean
+
+#problem: Please write a function named mean, which takes three integer arguments.
+#The function should print out the arithmetic mean of the three arguments.
+
+#mean(5, 3, 1)
+#mean(10, 1, 1)
+
+#3.0
+#4.0
+
+#Solution:
+
+def mean(x, y, z):
+    result = (x + y + z)/3
+    print(result)
+
+
+if __name__ == "__main__":
+    mean(1, 2, 3)
