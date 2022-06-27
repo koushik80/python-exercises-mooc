12e4e35d2063ba9b4b613ed14ef439a6d28b40b0
koushik <koushik.dey@live.com>
2022-06-28 00:33:59 +0300

    :white_check_mark: e4.2.1s
    done

---
+++ p4/More Functions/e4.2.1.py
@@ -0,0 +1,33 @@
+#E-4.2.1:Line
+
+#Problem: Please write a function named line,
+#which takes two arguments: an integer and a string.
+#The function prints out a line of text, the length of which is specified by the first argument.
+#The character used to draw the line should be the first character in the second argument.
+#If the second argument is an empty string, the line should consist of stars.
+
+#An example of expected behaviour:
+
+        #line(7, "%")
+        #line(10, "LOL")
+        #line(3, "")
+
+#Sample output:
+
+        #%%%%%%%
+        #LLLLLLLLLL
+        #***
+
+#Solution:
+
+def line(num, str):
+    if str == "":
+        str = "*"
+        print(num*str)
+
+    else:
+        print(num*str[0])
+
+
+if __name__ == "__main__":
+    line(5, "x")
