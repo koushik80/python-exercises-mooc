5c7574584a38d1afdde9754c0afd8918298b05ea
koushik <koushik.dey@live.com>
2022-06-28 19:47:50 +0300

    :construction: awarded 30/34 points
    und

--- p3/Defining Functions/e3.4.4.py
+++ p3/Defining Functions/e3.4.4.py
@@ -26,5 +26,12 @@
 
 #Solution:
 
+def print_many_times(text, times):
+    while times > 0:
+        print(text)
+        times -= 1
 
+
+if __name__ == "__main__":
+    print_many_times("python", 5)
 
