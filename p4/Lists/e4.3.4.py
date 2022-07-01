fcf84e786fa0da39fc13a3c36df3784a387f8b1b
koushik <koushik.dey@live.com>
2022-07-01 13:29:35 +0300

    :white_check_mark: e4.3.4s
    git push

--- p4/Lists/e4.3.4.py
+++ p4/Lists/e4.3.4.py
@@ -1,9 +1,47 @@
 #E4.3.4: Same word twice
 
-#Problem:
+#Problem:Please write a program which asks the user for words.
+#If the user types in a word for the second time,
+#the program should print out the number of different words typed in, and exit.
 
 
 #Sample output:
 
+        #Word: once
+        #Word: upon
+        #Word: a
+        #Word: time
+        #Word: upon
+        #You typed in 4 different words
+
+#NB: this exercise doesn't ask you to write any functions,
+#so you should not place any code within an if __name__ == "__main__" block.
 
 #Solution
+
+lst = []
+
+while True:
+    w = input("Word: ")
+    if w in lst:
+        break
+    lst.append(w)
+print(f'You typed in {len(lst)} different words')
+
+
+
+
+#different way:
+
+#lst = []
+#i = 0
+
+#while True:
+    #w = input("Word: ")
+
+    #if w in lst:
+        #print(f'You typed in {i} different words')
+        #break
+    #else:
+        #lst.append(w)
+        #i += 1
