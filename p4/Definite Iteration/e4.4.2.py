b635ee71457e66ab9201b67b561f58a34d80d2bb
koushik <koushik.dey@live.com>
2022-07-02 20:30:33 +0300

    :white_check_mark: e4.4.2s
    d

--- p4/Definite Iteration/e4.4.2.py
+++ p4/Definite Iteration/e4.4.2.py
@@ -2,7 +2,8 @@
 
 #Problem: Please write a program which asks the user for a positive integer N.
 #The program then prints out all numbers between -N and N inclusive,
-#but leaves out the number 0. Each number should be printed on a separate line.
+#but leaves out the number 0.
+#Each number should be printed on a separate line.
 
 #An example of expected behaviour:
 
