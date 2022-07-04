0f53ab9c2999846372c1bc377dfa79f321598422
koushik <koushik.dey@live.com>
2022-07-04 21:35:15 +0300

    :white_check_mark: e4.4.12s
    d

--- p4/Definite Iteration/e4.4.12.py
+++ p4/Definite Iteration/e4.4.12.py
@@ -1,9 +1,42 @@
-#E-4.4.1:All the longest in the list
+# E-4.4.1:All the longest in the list
 
-#Problem:
+# Problem:Please write a function named all_the_longest,
+# which takes a list of strings as its argument.
+# The function should return a new list containing the longest string in the original list.
+# If more than one are equally long,
+# the function should return all of the longest strings.
+
+# The order of the strings in the returned list should be the same as in the original.
+
+#my_list = ["first", "second", "fourth", "eleventh"]
+
+#result = all_the_longest(my_list)
+# print(result)  # ['eleventh']
+
+# -----------------------------------------------------
+
+#my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+#result = all_the_longest(my_list)
+# print(result)  # ['dorothy', 'richard']
+
+# Sample output:
 
 
-#Sample output:
+# Solution:
+
+def all_the_longest(lst: list):
+    longest = len(max(lst, key=len))
+    new_list = []
+    l = len(lst)
+
+    for i in range(l):
+        if (len(lst[i]) == longest):
+            new_list.append(lst[i])
+    return new_list
 
 
-#Solution:
+if __name__ == '__main__':
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = all_the_longest(my_list)
+    print(result)  # ['dorothy', 'richard']
