352f30b4a6d56b1d4facc2c53fa6c2cc15ac0490
koushik <koushik.dey@live.com>
2022-07-04 13:22:24 +0300

    :white_check_mark: e4.4.3s
    d

--- p4/Definite Iteration/e4.4.3.py
+++ p4/Definite Iteration/e4.4.3.py
@@ -1,9 +1,26 @@
 #E-4.4.1:List of stars
 
-#Problem:
+#Problem: Please write a function named list_of_stars,
+#which takes a list of integers as its argument.
+#The function should print out lines of star characters.
+#The numbers in the list specify how many stars each line should contain.
 
+#For example, with the function call list_of_stars([3, 7, 1, 1, 2]) the following should be printed out:
 
 #Sample output:
 
+        #***
+        #*******
+        #*
+        #*
+        #**
 
 #Solution:
+
+def list_of_stars(lst: list):
+    for i in lst:
+        print(i * "*")
+
+
+if __name__ == '__main__':
+    list_of_stars([3, 7, 1, 1, 2])
