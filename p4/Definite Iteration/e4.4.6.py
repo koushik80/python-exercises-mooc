f9e5cce8152cdcb759590a39f0407730633bbdbb
koushik <koushik.dey@live.com>
2022-07-04 15:38:30 +0300

    :white_check_mark: e4.4.6s
    d

--- p4/Definite Iteration/e4.4.6.py
+++ p4/Definite Iteration/e4.4.6.py
@@ -1,9 +1,30 @@
 #E-4.4.1:The sum of positive numbers
 
-#Problem:
+#Problem:Please write a function named sum_of_positives,
+#which takes a list of integers as its argument.
+#The function returns the sum of the positive values in the list.
 
+        #my_list = [1, -2, 3, -4, 5]
+        #result = sum_of_positives(my_list)
+        #print("The result is", result)
 
 #Sample output:
 
+        #The result is 9
 
 #Solution:
+
+def sum_of_positives(lst :list):
+
+    positive_num = []
+
+    for i in lst:
+        if i > 0:
+            positive_num.append(i)
+    return sum(positive_num)
+
+
+if __name__ == '__main__':
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
