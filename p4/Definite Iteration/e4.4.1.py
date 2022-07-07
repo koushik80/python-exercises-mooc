e1c4dcdb031dce7970d7ca5506e168d2225a15cc
koushik <koushik.dey@live.com>
2022-07-08 00:46:36 +0300

    :white_check_mark: awarded 37/38 points
    d

--- p4/Definite Iteration/e4.4.1.py
+++ p4/Definite Iteration/e4.4.1.py
@@ -1,30 +1,31 @@
-#E-4.4.1:Star-studded
+# E-4.4.1:Star-studded
 
-#Problem: Please write a program which asks the user to type in a string.
-#The program then prints each input character on a separate line.
-#After each character there should be a star (*) printed on its own line.
+# Problem: Please write a program which asks the user to type in a string.
+# The program then prints each input character on a separate line.
+# After each character there should be a star (*) printed on its own line.
 
-#This is how it should work:
+# This is how it should work:
 
-#Sample output:
+# Sample output:
 
-        #Please type in a string: Python
-        #P
-        #*
-        #y
-        #*
-        #t
-        #*
-        #h
-        #*
-        #o
-        #*
-        #n
-        #*
+# Please type in a string: Python
+# P
+# *
+# y
+# *
+# t
+# *
+# h
+# *
+# o
+# *
+# n
+# *
 
-#NB: this exercise doesn't ask you to write any functions,
-#so you should not place any code within an if __name__ == "__main__" block.
-#Solution:
+# NB: this exercise doesn't ask you to write any functions,
+# so you should not place any code within an if __name__ == "__main__" block.
+# Solution:
+
 
 user_input = input("Please type in a string: ")
 
