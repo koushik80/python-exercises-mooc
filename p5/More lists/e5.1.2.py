45f1ac5bb7611d24a81a94e73d89464d92bb6c4f
koushik <koushik.dey@live.com>
2022-07-08 18:28:56 +0300

    :white_check_mark: e5.1.2s
    d

--- p5/More lists/e5.1.2.py
+++ p5/More lists/e5.1.2.py
@@ -15,3 +15,17 @@
 # 3
 
 # Solution:
+
+def count_matching_elements(my_matrix: list, element: int):
+    item_match = 0
+    for row in my_matrix:
+        for num in row:
+            if element == num:
+                item_match += 1
+
+    return item_match
+
+
+if __name__ == '__main__':
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
