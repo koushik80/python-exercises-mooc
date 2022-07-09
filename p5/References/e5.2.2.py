e6df3c5afad0af8349378aa5282556579487e413
koushik <koushik.dey@live.com>
2022-07-10 00:42:31 +0300

    p2

--- p5/References/e5.2.2.py
+++ p5/References/e5.2.2.py
@@ -1,6 +1,19 @@
 # E-5.2.2: Remove the smallest
 
-# Problem:
+# Problem:Please write a function named remove_smallest(numbers: list),
+# which takes a list of integers as its argument.
+
+# The functions should find and remove the smallest item in the list.
+# You may assume there is a single smallest item in the list.
+
+# The function should not have a return value - it should directly modify the list it receives as a parameter.
+
+# An example of how the function works:
+
+# if __name__ == "__main__":
+#numbers = [2, 4, 6, 1, 3, 5]
+# remove_smallest(numbers)
+# print(numbers)
 
 
 # Sample output:
