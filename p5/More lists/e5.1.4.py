1b8a18822c949158d91803a7e835d7a5eb640daa
koushik <koushik.dey@live.com>
2022-07-09 21:24:43 +0300

    :white_check_mark: e5.1.4s
    d

--- p5/More lists/e5.1.4.py
+++ p5/More lists/e5.1.4.py
@@ -1,8 +1,12 @@
 # E-5.1.4: Sudoku: check now
 
-# Problem:Please write a function named row_correct(sudoku: list, row_no: int), which takes a two-dimensional array representing a sudoku grid, and an integer referring to a single row, as its arguments. Rows are indexed from 0.
+# Problem:Please write a function named row_correct(sudoku: list, row_no: int),
+# which takes a two-dimensional array representing a sudoku grid,
+# and an integer referring to a single row, as its arguments.
+# Rows are indexed from 0.
 
-# The function should return True or False, depending on whether the row is filled in correctly, that is, whether it contains each of the numbers 1 to 9 at most once.
+# The function should return True or False, depending on whether the row is filled in correctly,
+# that is, whether it contains each of the numbers 1 to 9 at most once.
 
 # sudoku = [
 #[9, 0, 0, 0, 8, 0, 3, 0, 0],
@@ -26,3 +30,31 @@
 # False
 
 # Solution:
+
+def row_correct(sudoku: list, row_no: int):
+    row = sudoku[row_no]
+    numbers = []
+
+    for num in row:
+        if num > 0 and num in numbers:
+            return False
+        numbers.append(num)
+
+    return True
+
+
+if __name__ == '__main__':
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(row_correct(sudoku, 0))
+    print(row_correct(sudoku, 1))
