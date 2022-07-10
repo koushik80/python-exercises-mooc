0b8e7ecd8ae13833a45fb6c27790a5de9fa569c8
koushik <koushik.dey@live.com>
2022-07-11 01:18:25 +0300

    dict-q2

--- p5/Dictionary/e5.3.8.py
+++ p5/Dictionary/e5.3.8.py
@@ -1,9 +1,29 @@
 # E-5.3.8: Movie database
 
-# Problem
+# Problem: Please write a function named add_movie(database: list, name: str, director: str, year: int, runtime: int),
+# which adds a new movie object into a movie database.
+
+# The database is a list,
+# and each movie object in the list is a dictionary.
+# The dictionary should contain the following keys.
+
+# name
+# director
+# year
+# runtime
+# The values attached to these keys are given as arguments to the function.
+
+# An example of its use:
+
+#database = []
+#add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+#add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+# print(database)
 
 
 # Sample output:
 
+# [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+# {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94}]
 
 # Solution:
--- p5/Dictionary/e5.3.9.py
+++ p5/Dictionary/e5.3.9.py
@@ -1,9 +1,26 @@
 # E-5.3.9: Find movies
 
-# Problem
+# Problem: Please write a function named find_movies(database: list, search_term: str),
+# which processes the movie database created in the previous exercise.
+# The function should formulate a new list,
+# which contains only the movies whose title includes the word searched for.
+# Capitalisation is irrelevant here.
+# A search for ana should return a list containing both Anaconda and Management.
+
+# An example of its use:
+
+# database = [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+# {"name": "Pythons on a Plane", "director": "Renny Pytholin",
+# "year": 2001, "runtime": 94},
+# {"name": "Dawn of the Dead Programmers", "director": "M. Night Python", "year": 2011, "runtime": 101}]
+
+#my_movies = find_movies(database, "python")
+# print(my_movies)
 
 
 # Sample output:
 
+# [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+# {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94}]
 
 # Solution:
