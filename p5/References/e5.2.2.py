c3dbe60bba670927bba1111212465490b8cb600d
koushik <koushik.dey@live.com>
2022-07-10 18:01:23 +0300

    :white_check_mark: e5.2.2s
    d

--- p5/References/e5.2.2.py
+++ p5/References/e5.2.2.py
@@ -18,5 +18,16 @@
 
 # Sample output:
 
+#[2, 4, 6, 3, 5]
 
 # Solution:
+
+def remove_smallest(numbers: list):
+    smallest = min(numbers)
+    numbers.remove(smallest)
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
