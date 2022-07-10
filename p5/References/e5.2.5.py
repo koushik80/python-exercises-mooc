0a234ea023b724685815c756415f672b75427bc3
koushik <koushik.dey@live.com>
2022-07-10 20:44:42 +0300

    :white_check_mark: e5.2.5s
    d

--- p5/References/e5.2.5.py
+++ p5/References/e5.2.5.py
@@ -33,3 +33,19 @@
 #[['', '', 'X'], ['', '', ''], ['', '', '']]
 
 # Solution:
+
+def play_turn(game_board: list, x: int, y: int, piece: str):
+
+    if x in range(0, 3) and y in range(0, 3):
+        if game_board[y][x] == "":
+            if piece == "X" or piece == "O":
+                game_board[y][x] = piece
+                return True
+
+    return False
+
+
+if __name__ == "__main__":
+    game_board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    print(play_turn(game_board, 2, 0, "X"))
+    print(game_board)
