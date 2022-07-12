df4771fc0f5ece25641bf1a4413f22d2e5770b43
koushik <koushik.dey@live.com>
2022-07-12 20:50:31 +0300

    :white_check_mark: e5.3.2s
    d

--- p5/Dictionary/e5.3.2.py
+++ p5/Dictionary/e5.3.2.py
@@ -23,3 +23,18 @@
 # 120
 
 # Solution:
+
+def factorials(n: int):
+    k = {}
+    x = 1
+    for i in range(1, n + 1):
+        x *= i
+        k[i] = x
+    return k
+
+
+if __name__ == '__main__':
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
