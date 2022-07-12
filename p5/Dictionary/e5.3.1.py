268cc583160586f0d89b6c40afb2141dede95027
koushik <koushik.dey@live.com>
2022-07-12 19:44:53 +0300

    :white_check_mark: e5.3.1s
    d

--- p5/Dictionary/e5.3.1.py
+++ p5/Dictionary/e5.3.1.py
@@ -16,3 +16,16 @@
 #{3: 30, 4: 40, 5: 50, 6: 60}
 
 # Solution:
+
+def times_ten(start_index: int, end_index: int):
+
+    my_dict = {}
+    for i in range(start_index, end_index + 1):
+        my_dict[i] = i * 10
+
+    return my_dict
+
+
+if __name__ == '__main__':
+    d = times_ten(3, 6)
+    print(d)
