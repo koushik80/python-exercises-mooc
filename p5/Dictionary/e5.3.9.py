9c88271647cf346f93baa233fed54e5f55bd76b0
koushik <koushik.dey@live.com>
2022-07-13 19:07:42 +0300

    :white_check_mark: e5.3.9s
    d

--- p5/Dictionary/e5.3.9.py
+++ p5/Dictionary/e5.3.9.py
@@ -24,3 +24,38 @@
 # {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94}]
 
 # Solution:
+
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+
+    dictionary = {}
+    n = "name"
+    d = "director"
+    y = "year"
+    r = "runtime"
+
+    dictionary[n] = "name"
+    dictionary[d] = "director"
+    dictionary[y] = "year"
+    dictionary[r] = "runtime"
+    database.append(dictionary)
+
+
+def find_movies(database: list, search_term: str):
+
+    my_movies = []
+
+    for dictionary in database:
+        if search_term.lower() in dictionary["name"].lower():
+            my_movies.append(dictionary)
+
+    return my_movies
+
+
+if __name__ == '__main__':
+    database = [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+                {"name": "Pythons on a Plane", "director": "Renny Pytholin",
+                    "year": 2001, "runtime": 94},
+                {"name": "Dawn of the Dead Programmers", "director": "M. Night Python", "year": 2011, "runtime": 101}]
+
+    my_movies = find_movies(database, "python")
+    print(my_movies)
