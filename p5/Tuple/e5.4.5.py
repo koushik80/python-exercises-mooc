8b412f9e99db0490be967b01f01eab250d47c3e6
koushik <koushik.dey@live.com>
2022-07-13 19:41:26 +0300

    p5q

--- p5/Tuple/e5.4.5.py
+++ p5/Tuple/e5.4.5.py
@@ -1,9 +1,35 @@
 # E-5.4.5: A square of letters
 
-# Problem:
+# Problem:This final exercise in this part is a relatively demanding problem solving task.
+# It can be solved in many different ways.
+# Even though this current section in the material covers tuples,
+# tuples are not necessarily the best way to go about solving this.
+
+# Please write a program which prints out a square of letters as specified in the examples below.
+# You may assume there will be at most 26 layers.
 
 
 # Sample output:
 
+#Layers: 3
+# CCCCC
+# CBBBC
+# CBABC
+# CBBBC
+# CCCCC
+
+# Sample output:
+
+#Layers: 4
+# DDDDDDD
+# DCCCCCD
+# DCBBBCD
+# DCBABCD
+# DCBBBCD
+# DCCCCCD
+# DDDDDDD
+
+# NB: this exercise doesn't ask you to write any functions,
+# so you should not place any code within an if __name__ == "__main__" block.
 
 # Solution:
