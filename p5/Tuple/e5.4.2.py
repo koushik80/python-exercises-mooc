8c4da77147aafef5619251f2d78da305e908c0b8
koushik <koushik.dey@live.com>
2022-07-13 22:35:41 +0300

    :white_check_mark: e5.4.2s
    d

--- p5/Tuple/e5.4.2.py
+++ p5/Tuple/e5.4.2.py
@@ -22,3 +22,27 @@
 
 
 # Solution:
+
+def oldest_person(people: list):
+
+    oldest_person = ()
+
+    for person in people:
+        l = len(oldest_person)
+        if l == 0:
+            oldest_person = people[0]
+        elif l > 0:
+            if person[1] < oldest_person[1]:
+                oldest_person = person
+
+    return oldest_person[0]
+
+
+if __name__ == '__main__':
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    print(oldest_person(people))
