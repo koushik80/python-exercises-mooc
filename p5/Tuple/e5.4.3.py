2b59f9979fd2d1c80fb201d19634a0f2b316dc56
koushik <koushik.dey@live.com>
2022-07-13 23:13:16 +0300

    :white_check_mark: e5.4.3s
    d

--- p5/Tuple/e5.4.3.py
+++ p5/Tuple/e5.4.3.py
@@ -23,3 +23,27 @@
 #[ 'Adam', 'Mary' ]
 
 # Solution:
+
+def older_people(people: list, year: int):
+
+    olders = []
+    #l = len(people)
+
+    for i in range(len(people)):
+        for j in range(len(people[i])):
+            if j == 1:
+                if people[i][j] < year:
+                    olders.append(people[i][0])
+
+    return olders
+
+
+if __name__ == '__main__':
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    older = older_people(people, 1979)
+    print(older)
