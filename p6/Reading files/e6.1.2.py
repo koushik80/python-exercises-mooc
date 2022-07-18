1a0847bb92b3e7000b8fb76e2e56d962c2379e24
koushik <koushik.dey@live.com>
2022-07-19 01:20:43 +0300

    :white_check_mark: e6.1.2s
    d

--- p6/Reading files/e6.1.2.py
+++ p6/Reading files/e6.1.2.py
@@ -22,3 +22,18 @@
 
 
 # Solution:
+
+def read_fruits():
+    fruits = {}
+    with open("fruits.csv") as new_file:
+        for line in new_file:
+            parts = line.split(";")
+            fruit = parts[0]
+            price = float(parts[1])
+            fruits[fruit] = price
+
+    return fruits
+
+
+if __name__ == '__main__':
+    print(read_fruits())
