f861192110fb499485ef69f310e73d1b10ece1e1
koushik <koushik.dey@live.com>
2022-07-20 02:48:31 +0300

    :white_check_mark: e6.1.4s
    d

--- p6/Reading files/e6.1.4.py
+++ p6/Reading files/e6.1.4.py
@@ -9,7 +9,7 @@
 # Solution:
 
 
-if False:
+if True:
     student_info = input("Student information: ")
     exercise_data = input("Exercises completed: ")
 else:
@@ -22,7 +22,7 @@ students = {}
 with open(student_info) as file:
     for line in file:
         parts = line.split(";")
-        if parts[0] == "student_id":
+        if parts[0] == "id":
             continue
         name = parts[1] + " " + parts[2].strip()
         students[parts[0]] = name
@@ -32,17 +32,17 @@ exercises = {}
 with open(exercise_data) as file:
     for line in file:
         parts = line.split(";")
-        if parts[0] == "student_id":
+        if parts[0] == "id":
             continue
         value = 0
         for i in range(1, len(parts)):
-            value += int(len(parts[i]))
+            value += int(parts[i])
         exercises[parts[0]] = value
 
 #print(("Student information: "), student_info)
 #print(("Exercises completed: "), exercise_data)
-print(students)
-print(exercises)
 
+# print(students)
+# print(exercises)
 for student_nr, name in students.items():
     print(f'{name} {exercises[student_nr]}')
