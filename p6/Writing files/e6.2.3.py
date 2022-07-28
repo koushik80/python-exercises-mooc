51260ab81cfcad3a25207eca882772fb4a2b7378
koushik <koushik.dey@live.com>
2022-07-29 00:38:19 +0300

    :white_check_mark: e6.2.3s
    d

--- p6/Writing files/e6.2.3.py
+++ p6/Writing files/e6.2.3.py
@@ -8,3 +8,38 @@
 
 
 # Solution:
+
+def filter_solutions():
+    name_of_student = []
+    point = []
+    problem = []
+    result = []
+
+    with open("solutions.csv") as new_file, open("correct.csv", "w") as correct, open("incorrect.csv", "w") as incorrect:
+        for line in new_file:
+            line = line.replace("\n", "")
+            parts = line.split(";")
+            name_of_student.append(parts[0])
+            point.append(parts[1])
+
+            if '+' in parts[1]:
+                points = parts[1].split("+")
+                total = int(points[0]) + int(points[1])
+                problem.append(total)
+            elif '-' in parts[1]:
+                points = parts[1].split("-")
+                difference = int(points[0]) - int(points[1])
+                problem.append(difference)
+            result.append(int(parts[2]))
+
+        for i in range(len(name_of_student)):
+            if problem[i] == result[i]:
+                correct.write(
+                    f'{name_of_student[i]};{point[i]};{result[i]}\n')
+            else:
+                incorrect.write(
+                    f'{name_of_student[i]};{point[i]};{result[i]}\n')
+
+
+if __name__ == '__main__':
+    filter_solutions()
