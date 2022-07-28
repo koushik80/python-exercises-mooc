345f610dcd0af05521820a10b8768b7884baf5cc
koushik <koushik.dey@live.com>
2022-07-29 02:12:49 +0300

    :white_check_mark: e6.2.7s
    d

--- p6/Writing files/e6.2.7.py
+++ p6/Writing files/e6.2.7.py
@@ -8,3 +8,30 @@
 
 
 # Solution:
+
+def backup_dictionary():
+    while True:
+        print("1 - Add word, 2 - Search, 3 - Quit")
+        function = int(input("Function: "))
+
+        if function == 1:
+            fi = input("The word in Finnish: ")
+            en = input("The word in English: ")
+            with open("dictionary.txt", "a") as new_file:
+                new_file.write(f'{str(fi)} - {str(en)}\n')
+            print("Dictionary entry added")
+
+        elif function == 2:
+            search = input("Search term: ")
+            with open("dictionary.txt") as new_file:
+                for line in new_file:
+                    if search.lower() in line.lower():
+                        line = line.strip()
+                        print(line)
+
+        elif function == 3:
+            print("Bye!")
+            break
+
+
+backup_dictionary()
