5f1406c20a461b5e19316629300ab39bc1619b10
koushik <koushik.dey@live.com>
2022-07-28 22:21:02 +0300

    :white_check_mark: e6.2.2s
    d

--- p6/Writing files/e6.2.2.py
+++ p6/Writing files/e6.2.2.py
@@ -8,3 +8,26 @@
 
 
 # Solution:
+
+def diary():
+    while True:
+        print("1 - add an entry, 2 - read entries, 0 - quit")
+        function = int(input("Function: "))
+
+        if function == 1:
+            entry = input("Diary entry: ")
+            with open("diary.txt", "a") as new_file:
+                new_file.write(f'{entry}\n')
+            print("Diary saved")
+
+        elif function == 2:
+            print("Entries:")
+            with open("diary.txt") as new_file:
+                print(new_file.read())
+
+        elif function == 0:
+            print("Bye now!")
+            break
+
+
+diary()
