e780eecae32027e2ad30cc20208e688540b1937f
koushik <koushik.dey@live.com>
2022-07-29 03:17:20 +0300

    :technologist: e6.3.2
    d

--- p6/Handling errors/e6.3.2.py
+++ p6/Handling errors/e6.3.2.py
@@ -1,6 +1,19 @@
-# E-6.3.2:
+# E-6.3.2: Parameter validation
 
-# Problem:
+# Problem: Please write a function named new_person(name: str, age: int),
+# which creates and returns a tuple containing the data in the arguments.
+# The first element should be the name and the second the age.
+
+# If the values stored in the parameter variables are not valid,
+# the function should throw a ValueError exception.
+
+# Invalid parameters in this case include:
+
+# name is an empty string
+# name contains less than two words
+# name is longer than 40 characters
+# age is a negative number
+# age is greater than 150
 
 
 # Sample output:
