30ba64eefcbcdbb5e4c561abe12e19257f98c5e1
koushik <koushik.dey@live.com>
2022-07-29 03:14:06 +0300

    :technologist: e6.3.1
    d

--- p6/Handling errors/e6.3.1.py
+++ p6/Handling errors/e6.3.1.py
@@ -1,9 +1,23 @@
-# E-6.3.1:
+# E-6.3.1: Reading Input
 
-# Problem:
+# Problem:Please write a function named read_input,
+# which asks the user for input until the user types in an integer which falls within the bounds given as arguments to the function.
+# The function should return the final valid integer value typed in by the user.
+
+# An example of the function in action:
+
+#number = read_input("Please type in a number: ", 5, 10)
+#print("You typed in:", number)
 
 
 # Sample output:
+
+# Please type in a number: seven
+# You must type in an integer between 5 and 10
+# Please type in a number: -3
+# You must type in an integer between 5 and 10
+# Please type in a number: 8
+# You typed in: 8
 
 
 # Solution:
