98784912d89c08559f5a0313dcf442678ac09f35
koushik <koushik.dey@live.com>
2022-08-02 18:12:34 +0300

    :white_check_mark: e7.1.2s
    d

--- p7/Modules/e7.1.2.py
+++ p7/Modules/e7.1.2.py
@@ -26,3 +26,30 @@
 
 
 # Solution:
+
+from string import ascii_letters, punctuation
+
+
+def separate_characters(my_string: str):
+    import string
+
+    eng_letters = ""
+    symbol = ""
+    diff_char = ""
+
+    for character in my_string:
+        if character in ascii_letters:
+            eng_letters += character
+        elif character in punctuation:
+            symbol += character
+        else:
+            diff_char += character
+
+    return eng_letters, symbol, diff_char
+
+
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])
