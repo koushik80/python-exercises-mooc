6869b6cbea6e2f5b2be18747aac5480d8e362a7f
koushik <koushik.dey@live.com>
2022-08-06 00:42:11 +0300

    Times and Dates

---
+++ p7/Times and Dates/e7.3.2.py
@@ -0,0 +1,9 @@
+# E-7.3.2: Valid PIC
+
+# Problem: https://programming-22.mooc.fi/part-7/3-times-and-dates#programming-exercise-how-old
+
+
+# Sample output:
+
+
+# Solution:
---
+++ p7/Times and Dates/e7.3.3.py
@@ -0,0 +1,9 @@
+# E-7.3.3: Screen Time
+
+# Problem: https://programming-22.mooc.fi/part-7/3-times-and-dates#programming-exercise-how-old
+
+
+# Sample output:
+
+
+# Solution:
