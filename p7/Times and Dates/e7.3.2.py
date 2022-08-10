e7dde740005c88ad3d07eed00cd408f29a6e9c17
koushik <koushik.dey@live.com>
2022-08-10 19:16:35 +0300

    :white_check_mark: e7.3.2s
    d

--- p7/Times and Dates/e7.3.2.py
+++ p7/Times and Dates/e7.3.2.py
@@ -7,3 +7,46 @@
 
 
 # Solution:
+from datetime import datetime
+from string import digits
+
+
+def is_it_valid(pic: str):
+    if len(pic) != 11:
+        return False
+
+    nums = pic[7:10]
+    for x in nums:
+        if x not in digits:
+            return False
+
+    remainder_index = "0123456789ABCDEFHJKLMNPRSTUVWXY"
+    remainder = int(pic[0:6] + pic[7:10]) % 31
+    if pic[10] != remainder_index[remainder]:
+        return False
+
+    yy = pic[6]
+    mm = pic[2:4]
+    dd = pic[0:2]
+
+    if '+' in yy:
+        year = '18' + pic[4:6]
+    elif '-' in yy:
+        year = '19' + pic[4:6]
+    elif 'A' in yy:
+        year = '20' + pic[4:6]
+    else:
+        return False
+
+    try:
+        dob = datetime(int(year), int(mm), int(dd))
+    except:
+        return False
+
+    return True
+
+
+if __name__ == '__main__':
+    print(is_it_valid("230827-906F"))
+    print(is_it_valid("120488+246L"))
+    print(is_it_valid("310823A9877"))
