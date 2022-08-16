33d64c00f7c2b6e200eec6bc28f05b5f09bb3b24
koushik <koushik.dey@live.com>
2022-08-16 16:29:34 +0300

    :white_check_mark: e7.4.3s
    d

--- p7/Data Processing/e7.4.3.py
+++ p7/Data Processing/e7.4.3.py
@@ -7,3 +7,36 @@
 
 
 # Solution:
+
+from datetime import datetime, timedelta
+import csv
+
+
+def cheaters():
+    with open("start_times.csv") as start, open("submissions.csv") as submission:
+        starting_time = {}
+        for line in csv.reader(start,  delimiter=";"):
+            name = line[0]
+            time = datetime.strptime(line[1], "%H:%M")
+            starting_time[name] = time
+
+        submission_time = {}
+        for line in csv.reader(submission,  delimiter=";"):
+            name = line[0]
+            time = datetime.strptime(line[3], "%H:%M")
+
+            if name not in submission_time:
+                submission_time[name] = time
+            elif time > submission_time[name]:
+                submission_time[name] = time
+
+        cheaters = []
+        for name in starting_time:
+            if submission_time[name] - starting_time[name] > timedelta(hours=3):
+                cheaters.append(name)
+
+        return cheaters
+
+
+if __name__ == '__main__':
+    print(cheaters())
