75d129ecea5a7f35a9c023a9156513473397acca
koushik <koushik.dey@live.com>
2022-08-16 15:13:41 +0300

    :white_check_mark: e7.4.2s
    d

--- p7/Data Processing/e7.4.2.py
+++ p7/Data Processing/e7.4.2.py
@@ -7,3 +7,66 @@
 
 
 # Solution:
+
+
+import urllib.request
+import certifi
+import json
+from math import floor
+#import ssl
+
+#Part: 1
+
+
+def retrieve_all():
+    #context = ssl._create_unverified_context()
+    address = "https://studies.cs.helsinki.fi/stats-mock/api/courses"
+    request = urllib.request.urlopen(address, cafile=certifi.where())
+    data = json.loads(request.read())
+    list = []
+
+    for valid in data:
+        active = valid["enabled"]
+        if active == False:
+            continue
+        course_name = valid["fullName"]
+        name = valid["name"]
+        year = valid["year"]
+        assignments = sum(valid["exercises"])
+        valid = course_name, name, year, assignments
+        list.append(valid)
+    return list
+
+#Part: 2
+
+
+def retrieve_course(course_name: str):
+    address = " https://studies.cs.helsinki.fi/stats-mock/api/courses/****/stats"
+    request = urllib.request.urlopen(address, cafile=certifi.where())
+    data = json.loads(request.read())
+    hours = 0
+    exercises = 0
+    students = 0
+
+    for x, y in data.items():
+        hours += y['hour_total']
+        exercises += y['exercise_total']
+        if y['students'] > students:
+            students = y['students']
+
+    hours_average = floor(hours/students)
+    exercises_average = floor(exercises/students)
+
+    return {
+        'weeks': len(data),
+        'students': students,
+        'hours': hours,
+        'hours_average': hours_average,
+        'exercises': exercises,
+        'exercises_average': exercises_average
+    }
+
+
+if __name__ == '__main__':
+    print(retrieve_all())
+    print(retrieve_course("docker2019"))
