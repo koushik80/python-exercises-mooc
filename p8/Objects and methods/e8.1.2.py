51fc4628f315b5df5f18650ab26cddbbdd807975
koushik <koushik.dey@live.com>
2022-08-24 01:28:01 +0300

    :white_check_mark: e8.1.2s
    d

--- p8/Objects and methods/e8.1.2.py
+++ p8/Objects and methods/e8.1.2.py
@@ -1,10 +1,21 @@
 # E-8.1.2: Row sums
 
 
-# Problem:
+# Problem: https://programming-22.mooc.fi/part-8/1-objects-and-methods#programming-exercise-row-sums
 
 
 # Sample output:
 
+#[[1, 2, 3], [3, 4, 7]]
 
 # Solution:
+
+def row_sums(my_matrix: list):
+    for row in my_matrix:
+        row.append(sum(row))
+
+
+if __name__ == '__main__':
+    my_matrix = [[1, 2], [3, 4]]
+    row_sums(my_matrix)
+    print(my_matrix)
