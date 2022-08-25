919d179f0b3eed0940459a0c7309befcea25ba25
koushik <koushik.dey@live.com>
2022-08-25 11:11:52 +0300

    Q

---
+++ Advanced_OOP/p8/Defining methods/e8.4.1.py
@@ -0,0 +1,20 @@
+# E-8.4.1: Decreasing counter
+
+# Problem: https://programming-22.mooc.fi/part-8/4-defining-methods#programming-exercise-decreasing-counter
+
+
+# Sample output:
+
+
+# Solution:
+
+# Part 1:
+
+
+# Part 2:
+
+
+# Part 3:
+
+
+# Part 4:
---
+++ Advanced_OOP/p8/Defining methods/e8.4.3.py
@@ -0,0 +1,10 @@
+# E-8.4.3: Statistics on numbers
+
+# Problem: https://programming-22.mooc.fi/part-8/4-defining-methods#programming-exercise-statistics-on-numbers
+
+
+# Sample output:
+
+# Numbers added: 4
+
+# Solution:
