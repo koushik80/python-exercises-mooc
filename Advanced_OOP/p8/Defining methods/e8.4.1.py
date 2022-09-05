be71c3be8a89302ffe36cc5f55e38a4249c7762f
koushik <koushik.dey@live.com>
2022-09-05 13:01:03 +0300

    :white_check_mark: e8.4.1s
    d

--- Advanced_OOP/p8/Defining methods/e8.4.1.py
+++ Advanced_OOP/p8/Defining methods/e8.4.1.py
@@ -8,13 +8,47 @@
 
 # Solution:
 
-# Part 1:
+
+# Tee ratkaisusi tähän:
+class DecreasingCounter:
+    def __init__(self, initial_value: int):
+        self.value = initial_value
+        self.initial = self.value
+
+    def print_value(self):
+        print("value:", self.value)
 
 
-# Part 2:
+    # Write the rest of the methods here!
+
+# Part 1 + 2:
+    def decrease(self):
+        if self.value > 0:
+            self.value -= 1
 
 
 # Part 3:
 
+    def set_to_zero(self):
+        self.value = 0
+
 
 # Part 4:
+
+    def reset_original_value(self):
+        self.value = self.initial
+
+
+
+
+if __name__ == '__main__':
+    counter = DecreasingCounter(10)
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.set_to_zero()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()
