6a064d5c6688e8a70be590122811092d21282677
koushik <koushik.dey@live.com>
2022-09-05 19:28:05 +0300

    :white_check_mark: e8.4.2s
    d

--- Advanced_OOP/p8/Defining methods/e8.4.2.py
+++ Advanced_OOP/p8/Defining methods/e8.4.2.py
@@ -30,3 +30,28 @@
 # Pythonnen
 
 # Solution:
+
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+    def return_first_name(self):
+        name = self.name.split(" ")
+        first_name = name[0]
+        return first_name
+
+    def return_last_name(self):
+        name = self.name.split(" ")
+        last_name = name[1]
+        return last_name
+
+
+if __name__ == "__main__":
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
+
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
